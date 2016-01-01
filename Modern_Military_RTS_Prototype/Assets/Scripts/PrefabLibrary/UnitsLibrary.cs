c86ce6852978e8f576ea7ed17d4995ba4cf71c9a
truoss <ruoss.work@gmx.de>
2016-01-01 15:17:36 +0100

    added units library

---
+++ Modern_Military_RTS_Prototype/Assets/Scripts/PrefabLibrary/UnitsLibrary.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[CreateAssetMenu(fileName = "PrefabLibrary", menuName = "PrefabLibrary", order = 0)]
+//[System.Serializable]
+public class UnitsLibrary : ScriptableObject
+{
+    static UnitsLibrary instance;
+    public static UnitsLibrary I
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Resources.Load<UnitsLibrary>("UnitsLibrary");
+
+                if (instance == null)
+                    Debug.LogError("Could not load prefab library, make sure it is exported with the built executeable!");
+
+                DontDestroyOnLoad(instance);
+            }
+
+            return instance;
+        }
+    }
+
+    public GameObject BaseUnit;    
+}
---
+++ Modern_Military_RTS_Prototype/Assets/Scripts/Units/BaseUnit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseUnit : MonoBehaviour
+{
+    public bool isMoving = false;
+
+    public float MovementSpeed;
+
+    public GameObject TargetField;
+    public GameObject CurrentField;
+}
