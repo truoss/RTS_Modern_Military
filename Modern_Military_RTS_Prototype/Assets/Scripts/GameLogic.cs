203dddd49a905eada99acf648325a257dbf1b876
truoss <ruoss.work@gmx.de>
2016-01-01 16:49:08 +0100

    added main class

---
+++ Modern_Military_RTS_Prototype/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameLogic : MonoBehaviour
+{
+    public static GameLogic I;
+    void Awake()
+    {
+        I = this;
+    }
+
+    public FieldManager FieldManager;
+
+    public Field SelectedField;
+    public Field LastSelectedField;
+
+    public BaseUnit SelectedUnit;
+    public BaseUnit LastSelectedUnit;
+
+    public List<BaseUnit> Units = new List<BaseUnit>();
+
+    void StartGame()
+    {
+        //Create Map here;
+        //place units here;
+    }
+
+    void FixedUpdate()
+    {
+        //Update Units Movement
+    }
+
+    void Update()
+    {
+        //Update Units
+    }
+}
--- Modern_Military_RTS_Prototype/Assets/Scripts/Units/BaseUnit.cs
+++ Modern_Military_RTS_Prototype/Assets/Scripts/Units/BaseUnit.cs
@@ -3,10 +3,40 @@ using System.Collections;
 
 public class BaseUnit : MonoBehaviour
 {
+    Renderer mesh;
+    public bool isSelected = false;
     public bool isMoving = false;
 
     public float MovementSpeed;
 
     public GameObject TargetField;
     public GameObject CurrentField;
+
+    Color baseColor;
+    void Start()
+    {
+        mesh = GetComponent<Renderer>();
+        baseColor = mesh.material.color;
+    }
+
+    //TODO:
+    //move from a to b
+    //select Unit
+
+    
+    
+    void OnMouseEnter()
+    {
+        mesh.material.color = Color.red;
+    }
+
+    void OnMouseOver()
+    {
+        mesh.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+    }
+
+    void OnMouseExit()
+    {
+        mesh.material.color = baseColor;
+    }
 }
